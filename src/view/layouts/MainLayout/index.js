a44ac7d3d68e72a3f925971380a0e449e85a0f04
tigran008 <tigran.s9.y@tumo.org>
2024-08-12 21:05:08 +0400

    fix errors

--- src/view/layouts/MainLayout/index.js
+++ src/view/layouts/MainLayout/index.js
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom';
 
-import Header from '../../components/global/Header';
+import { Header } from '../../components/global';
 import './index.css';
 
 const MainLayout = () => {
